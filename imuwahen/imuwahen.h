0bd4588d4ece4f81ee007eed85c3e6391d7303c3
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-03-14 20:59:59 +0100

    array to string function with method overloading

--- imuwahen/imuwahen.h
+++ imuwahen/imuwahen.h
@@ -3,14 +3,28 @@
 #ifndef IMUWAHEN_H
 #define IMUWAHEN_H
 
+template <typename T, int N>
+int getArrayLength (T(&)[N])
+{
+	return N;
+}
+
 class log
 {
 	private:
 		unsigned int len;
 	public:
-		static void insert(int arr[], int value, const unsigned int &length_of_array);
-		static void insert(std::string arr[], std::string value, const unsigned int &length_of_array);
+		static void insert(size_t arr[], size_t value, const int &length_of_array);
+		static void insert(int arr[], int value, const int &length_of_array);
+		static void insert(long int arr[], long int value, const int &length_of_array);
+		static void insert(long long int arr[], long long int value, const int &length_of_array);
+		static void insert(std::string arr[], std::string value, const int &length_of_array);
 		~log();
 };
+
+std::string to_json(std::string arr[], const int &length_of_array);
+std::string to_json(int arr[], const int &length_of_array);
+std::string to_json(long int arr[], const int &length_of_array);
+std::string to_json(long long int arr[], const int &length_of_array);
 
 #endif //IMUWAHEN_H
---
+++ imuwahen/json.cpp
@@ -0,0 +1,77 @@
+#include "imuwahen.h"
+#include <sstream>
+
+/* *
+ * to_json - program that convert array to JSON.
+ * @arr: array entry.
+ * @length_of_array: number of element in the array.
+ * Return: JSON string.
+ */
+
+std::string to_json(int arr[], const int &length_of_array)
+{
+	std::stringstream ss;
+	ss << "[";
+
+	for (int i = 0; i < length_of_array; i++)
+	{
+		ss << arr[i];
+
+		if (i != length_of_array - i)
+			ss << ",";
+	}
+	ss << "]";
+
+	return (ss.str());
+}
+
+std::string to_json(long int arr[], const int &length_of_array)
+{
+	std::stringstream ss;
+	ss << "[";
+
+	for (int i = 0; i < length_of_array; i++)
+	{
+		ss << arr[i];
+
+		if (i != length_of_array - i)
+			ss << ",";
+	}
+	ss << "]";
+
+	return (ss.str());
+}
+
+std::string to_json(long long int arr[], const int &length_of_array)
+{
+	std::stringstream ss;
+	ss << "[";
+
+	for (int i = 0; i < length_of_array; i++)
+	{
+		ss << arr[i];
+
+		if (i != length_of_array - i)
+			ss << ",";
+	}
+	ss << "]";
+
+	return (ss.str());
+}
+
+std::string to_json(std::string arr[], const int &length_of_array)
+{	
+	std::stringstream ss;
+	ss << "[";
+
+	for (int i = 0; i < length_of_array; i++)
+	{
+		ss << R"(")" << arr[i] << R"(")";
+
+		if (i != length_of_array - 1)
+			ss << ",";
+	}
+	ss << "]";
+
+	return (ss.str());
+}
